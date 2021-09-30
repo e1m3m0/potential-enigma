37e60c53478fec9bb4bcff498cb4e49dee659efa
e1m3m0 <gamateo@gmail.com>
2021-09-29 22:32:48 -0400

    write info to file

--- Develop/index.js
+++ Develop/index.js
@@ -1,17 +1,59 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: 'input',
     name: 'name',
+    message: 'What is your name?',
+    validate: projectInput => {
+      if (projectInput) {
+        return true;
+      } else {
+        console.log('Please enter your name!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'title',
     message: 'What is the name of your project?',
     validate: projectInput => {
       if (projectInput) {
         return true;
       } else {
         console.log('Please enter the name of your project!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your github?',
+    validate: projectInput => {
+      if (projectInput) {
+        return true;
+      } else {
+        console.log('Please enter the name of your github!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'project',
+    message: 'What is the name of your repository?',
+    validate: projectInput => {
+      if (projectInput) {
+        return true;
+      } else {
+        console.log("Please enter the name of your project's repository!");
         return false;
       }
     }
@@ -29,12 +71,12 @@ const questions = [
       }
     }
   },
-  {
-    type: 'checkbox',
-    name: 'languages',
-    message: 'What did you build this project with? (Check all that apply)',
-    choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-  },
+  // {
+  //   type: 'checkbox',
+  //   name: 'languages',
+  //   message: 'What did you build this project with? (Check all that apply)',
+  //   choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+  // },
   {
     type: 'input',
     name: 'install',
@@ -76,24 +118,62 @@ const questions = [
   },
   {
     type: 'list',
-    name: 'License',
+    name: 'license',
     message: "What is your project's license? (Choose from the provided list)",
-    choices: ['Apache 2.0', 'GNU GPLv3', 'GNU GPLv2', 'ISC', 'MPL 2.0', 'MIT', 'Other']
+    choices: ['Apache 2.0', 'GNU GPLv3', 'ISC', 'MPL 2.0', 'MIT', 'not licensed']
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Provide your contact email?',
+    validate: projectInput => {
+      if (projectInput) {
+        return true;
+      } else {
+        console.log('Please enter your email!');
+        return false;
+      }
+    }
   },
  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fileName, data, err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) {
+        reject(err);
+        // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+        return;
+      }
+
+      // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    });
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {
   return inquirer.prompt(questions);
-  
-  
 }
 
 // Function call to initialize app
 init()
 .then(data => {
-  console.log(data)
+  console.log(data);
+  return generateMarkdown(data);
+})
+.then(markdown => {
+  return writeToFile('./dist/README.md', markdown);
+})
+.then(writeToFileResponse => {
+  console.log(writeToFileResponse);
+})
+.catch(err => {
+  console.log(err);
 });
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,116 +1,86 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return`
-  ![${license}](https://img.shields.io/badge/license-${license}-brightgreen)
-  `
+  return`![${license}](https://img.shields.io/badge/license-${license.split(' ').join('_')}-brightgreen)`
  }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  const licenseList = [
-    {
-      name: "Apache 2.0",
-      link: "apache-2.0",      
-    },
-    {
-      name: "GNU GPLv3",
-      link: "gpl-3.0",
-    },
-    {
-      name: "ISC",
-      link: "isc",      
-    },
-    {
-      name: "MPL 2.0",
-      link: "mpl-2.0",      
-    },
-    {
-      name: "MIT",
-      link: "mit",      
-    },
-  ];
-  if (license !== "not licensed"){
-  const input =  licenseList.filter(selected => licenseList.name === license);
-  return`
-  https://choosealicense.com/licenses/${input.link}
-  `
-  }
-  return`https://choosealicense.com/licenses/`
+  return`https://choosealicense.com/licenses/${license.split(' ').join('-').toLowerCase()}`
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
+function renderLicenseSection(license, name) {
   return` 
   ${renderLicenseBadge(license)}
 
-  Copyright (c) ${new Date().getFullYear()} by ${input.name}. Licensed under the ![${licence}](${renderLicenseLink(license)})`
+  Copyright (c) ${new Date().getFullYear()} by ${name}. Licensed under the [${license}](${renderLicenseLink(license)})`
 }
   
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  const { license, languages, ...input } = data;
-  return `# [${input.title}](https://github.com/${input.github}/${input.project})
+  return `# ${data.title}
+  ------------------------
+
+  [GitHub Repo](https://github.com/${data.github}/${data.project})
 
   ## Description
   --------------
     
-    ${input.description}
+  ${data.description}
    
   ## Table of Content
   -------------------
 
-    * [Description](#description)
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributing](#contributing)
-    * [Test](#test)
-    * [Question](#questions)
+  1.   [Description](#description)
+  2.   [Installation](#installation)
+  3.   [Usage](#usage)
+  4.   [License](#license)
+  5.   [Contributing](#contributing)
+  6.   [Test](#test)
+  7.   [Question](#questions)
   
   ## Installation
   ---------------
 
-    ${input.installation}
+  ${data.install}
 
   ## Usage
   --------
 
-    ${input.usage}
+  ${data.usage}
 
   ## License
   ---------- 
     
-    ${renderLicenseSection(license)}
+  ${renderLicenseSection(data.license, data.name)}
   
   ## Contributing
   ---------------
     
   Please refer to each project's style and contribution guidelines for submitting patches and additions. In general, we follow the "fork-and-pull" Git workflow.
 
-    1. **Fork** the repo on GitHub
-    2. **Clone** the project to your own machine
-    3. **Commit** changes to your own branch
-    4. **Push** your work back up to your fork
-    5. Submit a **Pull request** so that we can review your changes
+  1. **Fork** the repo on GitHub
+  2. **Clone** the project to your own machine
+  3. **Commit** changes to your own branch
+  4. **Push** your work back up to your fork
+  5. Submit a **Pull request** so that we can review your changes
 
-    NOTE: Be sure to merge the latest from "upstream" before making a pull request!
+  NOTE: Be sure to merge the latest from "upstream" before making a pull request!
 
   ## Test
   -------
   
-  ${input.test}
+  ${data.test}
 
   ## Questions
 
-    Reach me at:
-      ### ${input.name}
-      
-      * [GitHub](https://github/${input.github})
-      * [Email](mailto:${input.email})
+  ### ${data.name}
+  *   [GitHub](https://github.com/${data.github})
+  *   [Email](mailto:${data.email})
 `;
 }
 
